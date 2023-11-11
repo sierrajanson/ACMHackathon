ce382e3bc6b2b5c77dada23f23e204eb164c318c
ryunzz <ryan.alexander.ni@gmail.com>
2023-11-11 04:55:08 -0800

    Search Bar adds

---
+++ calcraft/src/components/SearchResultsList.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const SearchResultsList = () => {
+    return (
+        <div className="results-list">SearchResultsList
+            <div>A</div>
+
+        </div>
+    )
+}
+
+export default SearchResultsList;
